945549888f9b6628a2fe0a59a64638052ee64ce7
shrishtikhurana <83277761+shrishtikhurana@users.noreply.github.com>
2021-07-07 14:15:02 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,35 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,	
+            restitution:0.3,
+            friction:0.5,
+            density:1.2		
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.image=loadImage("paper.png")
+		this.body=Bodies.circle(x, y, r , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+            var ang=this.body.angle
+			push()
+			translate(groundPos.x, groundPos.y);
+            rotate (ang)
+			//ellipseMode(RADIUS)
+			//strokeWeight(4);
+			fill(128,128,128)
+			image(this.image,0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
